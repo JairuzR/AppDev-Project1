13178894d1c4af88bbf84acfc6157b977ff46dd7
Jonnelle007 <gorgoniojonnelle67@gmail.com>
2025-03-14 19:48:37 +0800

    Update BankIdComparator.java

--- Project 1/Bank/BankIdComparator.java
+++ Project 1/Bank/BankIdComparator.java
@@ -1,9 +1,16 @@
 package Bank;
 
-public class BankIdComparator {
+import java.util.Comparator;
 
+public class BankIdComparator {
+    @Override
     public int compare(Bank b1, Bank b2) {
-        
+        if (b1.getID() == b2.getID() && b1.getPasscode().equals(b2.getPasscode())) {
+            return 0;
+        } 
+        else {
+            return 1;
+        }
     }
     
 }
